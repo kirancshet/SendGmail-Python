dd57d2d7fcd021cab476b94bb2d67defbb461f8d
kirancshet <kiran.cshet@gmail.com>
2022-10-11 14:58:48 +0530

    Updated the conf.py - Kiran

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # SendEmail
+
 This Python code snippet allows the user to send mail via Gmail
 
 # How to Run
+
 python send_email.py
-
-
--- conf.py
+++ conf.py
@@ -1,11 +1,10 @@
 
 
-#------------------------------------
+# ------------------------------------
 #   Gmail Settings
-#------------------------------------
+# ------------------------------------
 
-SMTPserver      = 'smtp.office365.com'
-gmail_username  = 'kiran.chandrashekhar@gmail.com'
-gmail_password  = '<gmail_password_here>'
+gmail_username = 'kiran.chandrashekhar@gmail.com'
+gmail_password = '<gmail_password_here>'
 gmail_smtp_host = 'smtp.gmail.com'
 port = 587
